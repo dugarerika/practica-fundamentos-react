60cfb67753591c23ab2b1d3356334ab319beb3f5
Erika Tavera <dugarerika@gmail.com>
2020-11-25 23:37:28 +0300

    creamos cliente para hacer peticiones al end point

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+// importamos la libreria de axios
+import axios from 'axios';
+
+// creamos un cliente que vamos a utilizar para hacer las peticiones a nuestro end points
+const cliente = axios.create({
+  baseUrl: process.env.REACT_APP_API_BASE_URL,
+});
+
 function App() {
   return <div className="App">Practica Fundamentos React</div>
 }
