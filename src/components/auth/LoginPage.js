373e8144ffad7e78dd5d5895d109acd35727be2f
Erika Dugar <dugarerika@gmail.com>
2021-01-06 20:42:46 +0300

    Creo la base para el formulario de login

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './LoginPage.css';
 
 class LoginPage extends React.Component {
 	render() {
-		<div className='LoginPage'>
-			<h1 className='LoginPage-tittle'>Log in the Twitter</h1>
-		</div>;
+		return (
+			<div className='LoginPage'>
+				<h1 className='LoginPage-tittle'>Log in the Twitter</h1>
+				<form>
+					<input type='text' />
+					<input type='password' />
+					<input type='submit' value='Log in' />
+				</form>
+			</div>
+		);
 	}
 }
 
