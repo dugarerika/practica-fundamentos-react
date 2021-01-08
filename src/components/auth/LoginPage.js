f4c97c80921282861da212651cb31c349b4a1855
Erika Dugar <dugarerika@gmail.com>
2021-01-08 17:17:26 +0300

    manejando los inputs en el logon page

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -3,14 +3,49 @@ import './LoginPage.css';
 import Button from '../shared/Button';
 
 class LoginPage extends React.Component {
+	state = {
+		form: {
+			email: '',
+			password: ''
+		}
+	};
+
+	haddleChange = (event) => {
+		console.log(event.target.value);
+		this.setState((state) => ({
+			form: { ...state.form, [event.target.name]: event.target.value }
+		}));
+	};
+
+	couldSubmit = () => {
+		const { form: { email, password } } = this.state;
+		return email && password;
+	};
+
 	render() {
+		const { form: { email, password } } = this.state;
+
 		return (
 			<div className='LoginPage'>
 				<h1 className='LoginPage-tittle'>Log in the Twitter</h1>
 				<form>
-					<input type='text' />
-					<input type='password' />
-					<Button type='submit' className='loginPage-button' variant='primary'>
+					<input
+						name='email'
+						type='text'
+						value={email}
+						onChange={this.haddleChange}
+					/>
+					<input
+						name='password'
+						type='password'
+						value={password}
+						onChange={this.haddleChange}
+					/>
+					<Button
+						type='submit'
+						className='loginPage-button'
+						variant='primary'
+						disabled={!this.couldSubmit()}>
 						Log In
 					</Button>
 				</form>
